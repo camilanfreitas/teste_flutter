4e580f11efbaa14ce6a2b6ab4a6ad5f30329055b
Camila Freitas <camiladonascimentofreitas@gmail.com>
2020-10-22 20:02:57 -0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(AppWidget(title: 'Flutter'));
+}
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          title,
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.white, fontSize: 65.0),
+        ),
+      ),
+    );
+  }
+}
